0f977b9e395d03c5c9fb29f8c4d7bc80fcccd54c
j1n3l0 <nelo.onyiah@gmail.com>
2011-03-18 01:35:29 +0000

    Add a fib var and solution to problem 002.

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,6 +1,18 @@
 (ns euler.core)
 
+;; Utility vars and functions ...
+
+(def fib
+  (lazy-cat [0 1] (map + fib (rest fib))))
+
+;; Solutions ...
+
 (defn problem-001
   "Solution to Euler problem 001"
   [n]
   (apply + (filter #(or (zero? (mod % 3)) (zero? (mod % 5))) (range n))))
+
+(defn problem-002
+  "Solution to Euler problem 002"
+  [n]
+  (reduce + (filter even? (take-while #(< % n) fib))))
--- test/euler/test/core.clj
+++ test/euler/test/core.clj
@@ -2,5 +2,13 @@
   (:use [euler.core] :reload)
   (:use [clojure.test]))
 
+(deftest test-fib
+  (is (= '(0 1 1 2) (take 4 fib))
+      "the first 4 fibonacci numbers should be '(0 1 1 2)"))
+
 (deftest test-problem-001
   (is (= 23 (problem-001 10)) "solution to (problem-001 10) should be 23"))
+
+(deftest test-problem-002
+  (is (= 10 (problem-002 10))
+      "solution to (problem-001 10) should be 10"))
